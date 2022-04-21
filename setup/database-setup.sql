0d146e16879c05232d1763b2c7f3e898b1a23697
S4more <guimsc@hotmail.com>
2022-04-21 09:30:43 -0400

    Remove locale from setup.

--- setup/database-setup.sql
+++ setup/database-setup.sql
@@ -4,8 +4,6 @@ CREATE DATABASE "academic-showcase-db"
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_Canada.1252'
-    LC_CTYPE = 'English_Canada.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
