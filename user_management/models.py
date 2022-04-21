f2abbcfb86a78612817003706de1ce34fba20329
Raagav Prasanna <raagav.pras@gmail.com>
2022-04-21 11:05:32 -0400

    Created model for profile and ImageFile which will be used for users avatar

--- user_management/models.py
+++ user_management/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # class 
 
 # Create your models here.
+
+class ImageFile(models.Model):
+    image = models.FileField()
+    image_data = models.BinaryField(null=True)
+
+class Profile(AbstractBaseUser):
+    name = models.CharField(max_length=254)
+    email_addr = models.EmailField(max_length=254)
+    avatar = ImageFile()
+
+
