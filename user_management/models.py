2fab5634a35365eab24f72b8bf82850ccf6ad251
Raagav Prasanna <raagav.pras@gmail.com>
2022-04-25 10:10:58 -0400

    changed avatar to simply store a BinaryField

--- user_management/models.py
+++ user_management/models.py
@@ -6,13 +6,9 @@ from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 
-class ImageFile(models.Model):
-    image = models.FileField()
-    image_data = models.BinaryField(null=True)
-
 class Profile(AbstractBaseUser):
     name = models.CharField(max_length=254)
     email_addr = models.EmailField(max_length=254)
-    avatar = ImageFile()
+    avatar = models.BinaryField()
 
 
