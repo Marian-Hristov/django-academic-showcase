74261df0a3838043c5be7982b868896b9e339cbc
Marian-Hristov <marianhr2014@gmail.com>
2022-04-30 19:16:58 -0400

    made url for each post

--- item_catalog/views.py
+++ item_catalog/views.py
@@ -2,9 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from django.views.generic.detail import DetailView
+
 from item_catalog.models import Post
 
 # Create your views here.
+
+def post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    comments = post.get_comments()
+    template = loader.get_template('item_catalog/post.html')
+    context = {
+        'post': post,
+        # 'comments': comments
+    }
+    return HttpResponse(template.render(context, request))
+
 def index(request):
     items = Post.objects.filter()
     template = loader.get_template('item_catalog/home.html')
