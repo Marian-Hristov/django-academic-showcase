6372409c3b883aaef483738abe9cbefc48cdbf29
Marian <marian.hristov@dawsoncollege.qc.ca>
2022-05-02 10:05:10 -0400

    fixed header styling for smaller displays

--- academic_showcase/urls.py
+++ academic_showcase/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('item_catalog.urls'), name='index')
+    path('item_catalog/', include('item_catalog.urls'), name='index')
 ]
