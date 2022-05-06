b96e407bcd4480d759338ba5bcd2b29ec720fecd
Marian <marian.hristov@dawsoncollege.qc.ca>
2022-05-06 15:26:57 -0400

    added debug and secret key os vars

--- academic_showcase/settings.py
+++ academic_showcase/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-712s**jmwqo&zb!)q5$fp(u)6eah-(o=6&bc1kdghx^&mbgcu_'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG  = os.environ['DEBUG'] == True
 
 ALLOWED_HOSTS = []
 
