948773c28f7bf16f330a1b6d36c371ad7f0741e0
Raagav Prasanna <raagav.pras@gmail.com>
2022-05-08 16:45:52 -0400

    Completed password reset functionality

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('register/', views.register_profile, name='register')
+    path('register/', views.register_profile, name='register'),
+    path('reset_password/', views.reset_pass, name='reset_password')
     # path('register/', views.UserRegisterView.as_view(), name='register')z
 ]
