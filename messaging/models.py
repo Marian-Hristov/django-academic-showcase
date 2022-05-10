cfa653c51b6bd17d80dd1f6ae11a342402ceb30c
Raagav Prasanna <raagav.pras@gmail.com>
2022-05-10 08:57:17 -0400

    Completed messaging models

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from user_management.models import Profile
 # Create your models here.
+
+class Message(models.Model):
+    body = models.TextField()
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE)
