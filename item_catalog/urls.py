debf927595a7941b03cdf52e1b5c734c18269177
Marian <marian.hristov@dawsoncollege.qc.ca>
2022-05-14 17:43:34 -0400

    starting to make detail/update/delete views for projects

--- item_catalog/urls.py
+++ item_catalog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/like/<str:action>', views.likeView, name="post-like"),
     path('projects/personal/<str:username>', views.ProjectListView.as_view(), name="projects"),
-    path('projects/personal/<str:username>/project/<int:pk>', views.ProjectListView.as_view(), name="project-detail"),
+    path('projects/personal/<str:username>/project/<int:pk>', views.ProjectDetailView.as_view(), name="project-detail"),
     path('projects/create/<str:username>', views.ProjectCreateView.as_view(), name="project-create"),
 ]
