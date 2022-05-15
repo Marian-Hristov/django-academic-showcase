838974388e4cde2765e46a46e4b36ef7ed0f1cef
Marian-Hristov <marianhr2014@gmail.com>
2022-05-15 10:34:14 -0400

    made member rating on posts

--- item_catalog/admin.py
+++ item_catalog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from item_catalog.models import Comment, Post, Project
+from item_catalog.models import Comment, Post, Project, Rating
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Comment)
 # admin.site.register(Post, PostAdmin)
 admin.site.register(Post)
+admin.site.register(Rating)
